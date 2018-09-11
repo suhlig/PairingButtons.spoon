93d071d93a2d16b3754a2735edf343d0772ed6ab
Steffen Uhlig <steffen@familie-uhlig.net>
2018-09-11 23:50:27 +0200

    First working version

---
+++ PairingButtons/PairingButtons.ino
@@ -0,0 +1,55 @@
+#include <Bounce.h>
+
+const byte SYSEX_BEGIN     = 0xF0;
+const byte SYSEX_END       = 0xF7;
+const byte MANUFACTURER_ID = 0x42;
+
+// Create Bounce objects for each button.  The Bounce object
+// automatically deals with contact chatter or "bounce", and
+// it makes detecting changes very simple.
+Bounce button7 = Bounce(7, 5);
+Bounce button8 = Bounce(8, 5);
+int led = 13;
+
+void setup() {
+  pinMode(7, INPUT_PULLUP);
+  pinMode(8, INPUT_PULLUP);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // Update all the buttons.  There should not be any long
+  // delays in loop(), so this runs repetitively at a rate
+  // faster than the buttons could be pressed and released.
+  button7.update();
+  button8.update();
+
+  if (button7.fallingEdge()) {
+    blink(50);
+    // see https://stackoverflow.com/a/15720219/3212907 and
+    // https://cnx.org/contents/csA1TDZU@3/MIDI-Messages#id-758884344759
+    byte data[] = { SYSEX_BEGIN, MANUFACTURER_ID, 0x07, SYSEX_END };
+    usbMIDI.sendSysEx(sizeof(data), data, true);
+  }
+
+  if (button8.fallingEdge()) {
+    blink(50);
+    byte data[] = { SYSEX_BEGIN, MANUFACTURER_ID, 0x08, SYSEX_END };
+    usbMIDI.sendSysEx(sizeof(data), data, true);
+  }
+
+  if (button7.risingEdge()) {
+  }
+
+  // MIDI Controllers should discard incoming MIDI messages.
+  // http://forum.pjrc.com/threads/24179-Teensy-3-Ableton-Analog-CC-causes-midi-crash
+  while (usbMIDI.read()) {
+    // ignore incoming messages
+  }
+}
+
+void blink(uint pause) {
+  digitalWrite(led, HIGH);
+  delay(pause);
+  digitalWrite(led, LOW);
+}
---
+++ init.lua
@@ -0,0 +1,78 @@
+--- === PairingButtons ===
+---
+--- Runs Hammerspoon code on pressing a physical button
+---
+--- In a pair programming session, this Spoon switches between each pair's
+--- settings on the press of a button. Two buttons are connected to a Teensy
+--- (or Arduino) which sends a MIDI SysEx message via USB when a button was
+--- presded. This Spoon applies whatever settings are associated with the button
+--- that was pressed, e.g. switching the keyboard layout.
+---
+--- Installation:
+--- 1. [Download the latest ZIP](https://github.com/suhlig/PairingButtons.spoon/releases)
+--- 2. Unzip it to `~/.hammerspoon/Spoons/PairingButtons.spoon`
+--- 3. Load it in `~/.hammerspoon/.init.lua` with `hs.loadSpoon("PairingButtons")`
+
+local obj = {}
+
+obj.__index = obj
+obj.name = "PairingButtons"
+obj.version = "1.0"
+obj.author = "Steffen Uhlig <steffen@familie-uhlig.net>"
+obj.homepage = "https://github.com/suhlig/PairingButtons.spoon"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+function obj:init()
+  midiDevice = hs.midi.new("Teensy MIDI")
+
+  if midiDevice then
+    midiDevice:callback(onMidiMessage)
+  else
+    hs.alert("Could not find MIDI device")
+  end
+end
+
+local function getSetting (label, default)
+  return hs.settings.get(obj.name .. "." .. label) or default
+end
+
+local function setSetting (label, value)
+  hs.settings.set(obj.name .. "." .. label, value); return value
+end
+
+local DEFAULT_DRIVER = "Steffen"
+
+function onMidiMessage(object, deviceName, commandType, description, metadata)
+  if metadata.manufacturerID == 0x42 then
+    driver = getSetting("driver", nil)
+
+    -- TODO Keep a table that maps key code to driver name
+    if metadata.sysexData == "07" then
+      if driver == nil then
+        driver = DEFAULT_DRIVER
+      elseif driver == "Steffen" then
+        hs.alert(driver .. " is still the driver")
+      else
+        driver = setSetting("driver", "Steffen")
+        hs.alert(driver .. " is now the driver")
+
+        -- TODO put into its own function or file, mapped by driver name
+        hs.keycodes.setLayout("German")
+      end
+    elseif metadata.sysexData == "08" then
+      if driver == nil then
+        driver = DEFAULT_DRIVER
+      elseif driver == "Julz" then
+        hs.alert(driver .. " is still the driver")
+      else
+        driver = setSetting("driver", "Julz")
+        hs.alert(driver .. " is now the driver")
+
+        -- TODO put into its own function or file, mapped by driver name
+        hs.keycodes.setLayout("U.S.")
+      end
+    end
+  end
+end
+
+return obj
